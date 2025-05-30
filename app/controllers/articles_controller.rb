90bbfe5ffbfd0268bed340ceda05ff7bd2ede4b2
manul <manuuva5@gmail.com>
2025-05-30 10:41:56 -0300

    add new view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,23 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:content)
+  end
 end
